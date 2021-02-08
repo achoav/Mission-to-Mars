37accaae3094410c3c499c659eab313cdca4d941
Valeria Achoa <73545138+achoav@users.noreply.github.com>
2021-02-08 14:57:35 -0500

    update

--- app.py
+++ app.py
@@ -6,9 +6,13 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
 import scraping_v2 as scraping
+
+# Setup Flask
 app = Flask(__name__)
+# Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
+
 #################################################
 # Flask Routes
 #################################################
